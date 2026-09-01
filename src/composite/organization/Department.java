5231b3a05aa51a6eccd0e904252d1467812e3551
NipaDelara <nipadelara@hotmail.com>
2026-09-02 02:51:40 +0300

    Task 3: Implement Department composite class with add, remove, salary calculation and XML output

--- src/composite/organization/Department.java
+++ src/composite/organization/Department.java
@@ -1,4 +1,39 @@
 package composite.organization;
 
-public class Department {
+import  java.util.ArrayList;
+import  java.util.List;
+
+public class Department implements OrganizationComponent{
+        private  String name;
+        private List<OrganizationComponent> members;
+        public Department(String name) {
+            this.name = name;
+            members = new ArrayList<>();
+        }
+        public void add(OrganizationComponent component) {
+            members.add(component);
+        }
+        public void remove(OrganizationComponent component) {
+            members.remove(component);
+        }
+
+    @Override
+    public double getSalary() {
+        double totalSalary = 0;
+        for (OrganizationComponent component : members) {
+            totalSalary += component.getSalary();
+        }
+        return totalSalary;
+    }
+
+    @Override
+    public void printXML(String indent) {
+
+        System.out.println(indent + "<department name=\"" + name + "\">");
+        for (OrganizationComponent component : members) {
+            component.printXML(indent + "  ");
+        }
+        System.out.println(indent + "</Department>");
+
+    }
 }
