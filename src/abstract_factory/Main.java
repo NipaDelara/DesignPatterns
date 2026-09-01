440e8125fbdd4787902ed8b5bdab751300b8ebb8
NipaDelara <nipadelara@hotmail.com>
2026-09-02 01:40:06 +0300

    Task 6: Add main application

--- src/abstract_factory/Main.java
+++ src/abstract_factory/Main.java
@@ -3,7 +3,23 @@ package abstract_factory;
 public class Main {
     public static void main(String[] args) {
 
+        // Choose Style A
+        UIFactory factory = new AFactory();
 
+        Button button = factory.createButton("Login");
+        TextField textField = factory.createTextField("Enter username");
+        Checkbox checkbox = factory.createCheckbox("Remember me");
+
+        System.out.println("STYLE A");
+        System.out.println();
+
+        button.display();
+        textField.display();
+        checkbox.display();
+
+        System.out.println();
+        System.out.println("After changing text:");
+        System.out.println();
 
     }
 }
