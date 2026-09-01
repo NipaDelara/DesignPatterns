d9d5958f0ca06211bc651c461f2ee29e4d9571a8
NipaDelara <nipadelara@hotmail.com>
2026-09-02 02:32:44 +0300

    Task 2: Add Employee leaf class

---
+++ src/composite/organization/Employee.java
@@ -0,0 +1,24 @@
+package composite.organization;
+
+public class Employee implements OrganizationComponent{
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public void printXML(String indent) {
+        System.out.println(
+                indent + "<employee name=\"" + name +
+                        "\" salary=\"" + salary + "\"/>"
+        );
+    }
+}
