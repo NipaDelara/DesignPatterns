ce261314a8769fd6b1de590a0bfe7df60251a7d4
NipaDelara <nipadelara@hotmail.com>
2026-09-08 13:23:33 +0300

    fixed error and add observer registration and removal

--- src/observer/weather_station/WeatherStation.java
+++ src/observer/weather_station/WeatherStation.java
@@ -6,19 +6,43 @@ import java.util.Random;
 
 public class WeatherStation extends Thread {
 
-    private final boolean running =true;
+    private final List<WeatherObserver> observers;
+    private final Random random;
+
+    private int temperature;
+
+    private final int MAX_TEMPERATURE = 35;
+    private final int MIN_TEMPERATURE = -10;
+
+    private boolean running = true;
 
     public WeatherStation() {
-        List<WeatherObserver> observers = new ArrayList<>();
-        Random random = new Random();
+        observers = new ArrayList<>();
+        random = new Random();
 
-
-        int MAX_TEMPERATURE = 35;
-        int MIN_TEMPERATURE = -10;
-        int temperature = random.nextInt(
+        temperature = random.nextInt(
                 MAX_TEMPERATURE - MIN_TEMPERATURE + 1
         ) + MIN_TEMPERATURE;
-        System.out.println("Initial temperature: "  + temperature +"°C");
+        System.out.println("Initial temperature: "  + temperature +"°C"
+        );
+    }
+    public void registerObserver(WeatherObserver observer) {
+
+        observers.add(observer);
+
+        System.out.println(
+                "Observer registered: "
+                        + observer.getClass().getSimpleName()
+        );
+    }
+    public void removeObserver(WeatherObserver observer) {
+
+        observers.remove(observer);
+
+        System.out.println(
+                "Observer removed: "
+                        + observer.getClass().getSimpleName()
+        );
     }
 
 }
