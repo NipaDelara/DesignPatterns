9a4d232b32478b714142a75dd67caeaad69a394c
NipaDelara <nipadelara@hotmail.com>
2026-09-22 19:00:15 +0300

    Implement DiceGame using Template Method pattern Add main class to test template method game

--- src/template_method/DiceGame.java
+++ src/template_method/DiceGame.java
@@ -1,4 +1,75 @@
 package template_method;
 
-public class DiceGame {
+import java.util.Scanner;
+
+public class DiceGame extends Game {
+
+    private int[] scores;
+    private int winner = -1;
+
+    private final int winningScore = 20;
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void initializeGame(int numberOfPlayers) {
+
+        scores = new int[numberOfPlayers];
+
+        System.out.println("Dice Race Game started!");
+        System.out.println("Number of players: " + numberOfPlayers);
+        System.out.println("First player to reach 20 points wins.");
+        System.out.println();
+    }
+
+    @Override
+    public boolean endOfGame() {
+        return winner != -1;
+    }
+
+    @Override
+    public void playSingleTurn(int player) {
+
+        System.out.print(
+                "Player " + (player + 1) +
+                        ", enter a number from 1 to 6: "
+        );
+
+        int number = scanner.nextInt();
+
+        while (number < 1 || number > 6) {
+            System.out.print(
+                    "Invalid number. Please enter 1 to 6: "
+            );
+
+            number = scanner.nextInt();
+        }
+
+        scores[player] += number;
+
+        System.out.println(
+                "Player " + (player + 1) +
+                        " entered: " + number
+        );
+
+        System.out.println(
+                "Player " + (player + 1) +
+                        " score: " + scores[player]
+        );
+
+        System.out.println();
+
+        if (scores[player] >= winningScore) {
+            winner = player;
+        }
+    }
+
+    @Override
+    public void displayWinner() {
+
+        System.out.println(
+                "Player " + (winner + 1) +
+                        " wins!"
+        );
+    }
 }
--- src/template_method/Main.java
+++ src/template_method/Main.java
@@ -2,6 +2,8 @@ package template_method;
 
 public class Main {
     public static void main(String[] args) {
+        Game game = new DiceGame();
 
+        game.play(2);
     }
 }
